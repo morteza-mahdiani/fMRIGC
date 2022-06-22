abb9bcdca1919de38d65542820c95718173e06d3
Morteza <m72.morteza@gmail.com>
2022-06-23 02:10:18 +0300

    adding class for gc parameters and use it in connectivity class

--- Connectivity.m
+++ Connectivity.m
@@ -1,22 +1,19 @@
 classdef Connectivity
     %Summary of this class
-    %   Detailed explanation 
     properties
         inputPathOfData
         inputPathOfMasks
         outputPath
         first_sub_ID
         last_sub_ID
-%         path_to_MVGC
     end
     properties
         sub_range
         regions
     end
 
-
     methods
-        function obj = Connectivity(pathOfData, pathOfMasks, outputPath, fID, lID, ROIs)
+        function obj = Connectivity(pathOfData, pathOfMasks, outputPath, fID, lID, ROIs, path_to_MVGC)
             obj.inputPathOfData = pathOfData;
             obj.inputPathOfMasks = pathOfMasks;
             obj.outputPath = outputPath;
@@ -24,7 +21,8 @@ classdef Connectivity
             obj.last_sub_ID = lID;
             obj.sub_range = (obj.first_sub_ID: obj.last_sub_ID);
             obj.regions = ROIs;
-%             obj.path_to_MVGC = path_to_MVGC;
+
+            obj.setPath(path_to_MVGC);
 
         end
         function preprocess(obj, TCDataLength)
@@ -61,22 +59,22 @@ classdef Connectivity
             end
         end
 
-        function GCM(obj)
-%             obj.setPath(obj.path_to_MVGC)
-            listOfsubjs = dir(append('/Users/saminjamshidi/Documents/condata/outG/', '*.mat'));
+        function GCM(obj, GC_param_obj, actual_model_order, preprocessed_data)
+            listOfsubjs = dir(append(preprocessed_data, '*.mat'));
             GC3DMat = nan(3, 3, length(obj.sub_range));
+            
             for n = 1 : length(listOfsubjs)
                 %% Parameters
-                ntrials   = 1;     % number of trials
-                nobs      = 1100;   % number of observations per trial
-                regmode   = 'OLS';  % VAR model estimation regression mode ('OLS', 'LWR' or empty for default)
-                icregmode = 'LWR';  % information criteria regression mode ('OLS', 'LWR' or empty for default)
-                morder    = 'AIC';  % model order to use ('actual', 'AIC', 'BIC' or supplied numerical value)
-                momax     = 20;     % maximum model order for model order estimation
-                tstat     = 'F';     % statistical test for MVGC:  'chi2' for Geweke's chi2 test (default) or'F' for Granger's F-test
-                alpha     = 0.05;   % significance level for significance test
-                mhtc      = 'FDR';  % multiple hypothesis test correction (see routine 'significance')
-                seed      = 0;      % random seed (0 for unseeded)
+                ntrials   = GC_param_obj.ntrials;     % number of trials
+                nobs      = GC_param_obj.nobs;   % number of observations per trial
+                regmode   = GC_param_obj.regmode;  % VAR model estimation regression mode ('OLS', 'LWR' or empty for default)
+                icregmode = GC_param_obj.icregmode;  % information criteria regression mode ('OLS', 'LWR' or empty for default)
+                morder    = GC_param_obj.morder;  % model order to use ('actual', 'AIC', 'BIC' or supplied numerical value)
+                momax     = GC_param_obj.momax;     % maximum model order for model order estimation
+                tstat     = GC_param_obj.tstat;     % statistical test for MVGC:  'chi2' for Geweke's chi2 test (default) or'F' for Granger's F-test
+                alpha     = GC_param_obj.alpha;   % significance level for significance test
+                mhtc      = GC_param_obj.mhtc;  % multiple hypothesis test correction (see routine 'significance')
+                seed      = GC_param_obj.seed;      % random seed (0 for unseeded)
 
                 %% Generate VAR test data
                 nvars = length(obj.regions); % number of variables
@@ -92,10 +90,10 @@ classdef Connectivity
                 load(sbj);
                 X = data;
                 ptoc;
-                name = split(listOfsubjs(n).name, '.');
-                %dirTosbj =  '/home/proactionlab/Documents/projects/connectivity/ToolsArea_outputs/' + string(name(1));
-                dirTosbj =  '/Users/saminjamshidi/Documents/condata/out/FaceArea_outputs/' + string(name(1));
-                mkdir(dirTosbj);
+                %name = split(listOfsubjs(n).name, '.');
+                %dirTosbj = obj.outputPath + '/GC_plots_and_outputs/' + string(name(1));
+                %mkdir(dirTosbj);
+
                 %% Model order estimation
                 % Calculate information criteria up to max model order
                 ptic('\n*** tsdata_to_infocrit\n');
@@ -104,11 +102,11 @@ classdef Connectivity
                 [~,bmo_AIC] = min(AIC);
                 [~,bmo_BIC] = min(BIC);
                 % Plot information criteria.
-%                 figure(1); clf;
-%                 plot((1:momax)',[AIC BIC]);
-%                 saveas(gcf,dirTosbj+'/'+string(name(1))+'_IC.png')
-%                 legend('AIC','BIC');
-                amo = 20; % actual model order
+                %                 figure(1); clf;
+                %                 plot((1:momax)',[AIC BIC]);
+                %                 saveas(gcf,dirTosbj+'/'+string(name(1))+'_IC.png')
+                %                 legend('AIC','BIC');
+                amo = actual_model_order; % actual model order
                 fprintf('\nbest model order (AIC) = %d\n',bmo_AIC);
                 fprintf('best model order (BIC) = %d\n',bmo_BIC);
                 fprintf('actual model order     = %d\n',amo);
@@ -145,58 +143,58 @@ classdef Connectivity
                 pval = mvgc_pval(F,morder,nobs,ntrials,1,1,nvars-2,tstat);
                 sig  = significance(pval,alpha,mhtc);
                 % Plot time-domain causal graph, p-values and significance.
-%                 figure(2); clf;
-%                 subplot(1,3,1);
-%                 plot_pw(F);
-%                 disp(pval);
-%                 title('Pairwise-conditional GC');
-%                 subplot(1,3,2);
-%                 plot_pw(pval);
-%                 title('p-values');
-%                 subplot(1,3,3);
-%                 plot_pw(sig);
-%                 title(['Significant at p = ' num2str(alpha)])
-%                 fprintf(2,'\nNOTE: no frequency-domain pairwise-conditional causality calculation in GCCA compatibility mode!\n');
-%                 saveas(gcf,dirTosbj+'/'+string(name(1))+'.png')
+                %                 figure(2); clf;
+                %                 subplot(1,3,1);
+                %                 plot_pw(F);
+                %                 disp(pval);
+                %                 title('Pairwise-conditional GC');
+                %                 subplot(1,3,2);
+                %                 plot_pw(pval);
+                %                 title('p-values');
+                %                 subplot(1,3,3);
+                %                 plot_pw(sig);
+                %                 title(['Significant at p = ' num2str(alpha)])
+                %                 fprintf(2,'\nNOTE: no frequency-domain pairwise-conditional causality calculation in GCCA compatibility mode!\n');
+                %                 saveas(gcf,dirTosbj+'/'+string(name(1))+'.png')
                 GC3DMat( :,: ,n)= pval;
+                %Free up used memory for other loops
                 clear bmo_BIC bmo_AIC X figure(1) figure(2);
             end
-            
-            %save(append(append(obj.outputPath, 'GC3Doutput_toolsAreas'), '.mat'),'GC3DMat')
+
             save(append(append(obj.outputPath, 'GC3Doutput_faceAreas'), '.mat'),'GC3DMat')
         end
     end
-    
-%     methods(Static)
-%         function setPath(path_to_MVGC)
-% 
-%             % Add mvgc root directory and appropriate subdirectories to path
-%             mvgc_root = fileparts(mfilename(path_to_MVGC)); % directory containing this file
-% 
-%             % essentials
-%             addpath(mvgc_root);
-%             addpath(fullfile(mvgc_root,'core'));
-%             addpath(fullfile(mvgc_root,'gc'));
-%             addpath(fullfile(mvgc_root,'gc','GCCA_compat'));
-%             addpath(fullfile(mvgc_root,'gc','subsample'));
-%             addpath(fullfile(mvgc_root,'stats'));
-%             addpath(fullfile(mvgc_root,'utils'));
-%             if ~fexists(@rng) || ~fexists(@randi) % legacy hack
-%                 addpath(fullfile(mvgc_root,'utils','legacy'));
-%                 if ~fexists(@rng),   addpath(fullfile(mvgc_root,'utils','legacy','rng'));   end
-%                 if ~fexists(@randi), addpath(fullfile(mvgc_root,'utils','legacy','randi')); end
-%             end
-%             addpath(fullfile(mvgc_root,'demo'));
-%             addpath(fullfile(mvgc_root,'mex'));
-%             addpath(fullfile(mvgc_root,'experimental'));
-%             addpath(fullfile(mvgc_root,'docs')); % don't add the 'html' subdirectory
-% 
-%             % comment out for release
-%             % addpath(fullfile(mvgc_root,'testing'));
-%             % addpath(fullfile(mvgc_root,'maintainer'));
-% 
-%             fprintf('[mvgc startup] Added MVGC root directory %s and subdirectories to path\n',mvgc_root);
-%         end
-%     end
+
+
+    methods(Static)
+        function setPath(path_to_MVGC)
+            % Add mvgc root directory and appropriate subdirectories to path
+            mvgc_root = fileparts(mfilename(path_to_MVGC)); % directory containing this file
+
+            % essentials
+            addpath(mvgc_root);
+            addpath(fullfile(mvgc_root,'core'));
+            addpath(fullfile(mvgc_root,'gc'));
+            addpath(fullfile(mvgc_root,'gc','GCCA_compat'));
+            addpath(fullfile(mvgc_root,'gc','subsample'));
+            addpath(fullfile(mvgc_root,'stats'));
+            addpath(fullfile(mvgc_root,'utils'));
+            if ~fexists(@rng) || ~fexists(@randi) % legacy hack
+                addpath(fullfile(mvgc_root,'utils','legacy'));
+                if ~fexists(@rng),   addpath(fullfile(mvgc_root,'utils','legacy','rng'));   end
+                if ~fexists(@randi), addpath(fullfile(mvgc_root,'utils','legacy','randi')); end
+            end
+            addpath(fullfile(mvgc_root,'demo'));
+            addpath(fullfile(mvgc_root,'mex'));
+            addpath(fullfile(mvgc_root,'experimental'));
+            addpath(fullfile(mvgc_root,'docs')); % don't add the 'html' subdirectory
+
+            % comment out for release
+            % addpath(fullfile(mvgc_root,'testing'));
+            % addpath(fullfile(mvgc_root,'maintainer'));
+
+            fprintf('[mvgc startup] Added MVGC root directory %s and subdirectories to path\n',mvgc_root);
+        end
+    end
 
 end
---
+++ GCParameters.m
@@ -0,0 +1,33 @@
+classdef GCParameters
+    %Summary of this class
+
+    properties
+        ntrials   = 1;      % number of trials
+        nobs      = 1100;   % number of observations per trial
+        regmode   = 'OLS';  % VAR model estimation regression mode ('OLS', 'LWR' or empty for default)
+        icregmode = 'LWR';  % information criteria regression mode ('OLS', 'LWR' or empty for default)
+        morder    = 'AIC';  % model order to use ('actual', 'AIC', 'BIC' or supplied numerical value)
+        momax     = 20;     % maximum model order for model order estimation
+        tstat     = 'F';    % statistical test for MVGC:  'chi2' for Geweke's chi2 test (default) or'F' for Granger's F-test
+        alpha     = 0.05;   % significance level for significance test
+        mhtc      = 'FDR';  % multiple hypothesis test correction (see routine 'significance')
+        seed      = 0;      % random seed (0 for unseeded)
+    end
+
+    methods
+        %class constructor
+        function obj = GCParameters(ntrials,nobs, regmode, icregmode, morder, momax, tstat, alpha, mhtc, seed)
+            obj.ntrials   = ntrials;
+            obj.nobs      = nobs;
+            obj.regmode   = regmode;
+            obj.icregmode = icregmode;
+            obj.morder    = morder;
+            obj.momax     = momax;
+            obj.tstat     = tstat;
+            obj.alpha     = alpha;
+            obj.mhtc      = mhtc;
+            obj.seed      = seed;
+        end
+
+    end
+end
