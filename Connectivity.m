787804591c98f015070b214bf8138a46b9327636
Morteza <m72.morteza@gmail.com>
2022-08-19 12:41:03 -0400

    activate not saving preprocessed data

--- Connectivity.m
+++ Connectivity.m
@@ -6,7 +6,8 @@ classdef Connectivity
         outputPath
         first_sub_ID
         last_sub_ID
-%         prepDataList = containers.Map
+        prepDataList
+        
     end
     properties
         sub_range
@@ -28,6 +29,8 @@ classdef Connectivity
             obj.last_sub_ID = lID;
             obj.sub_range = (obj.first_sub_ID: obj.last_sub_ID);
             obj.regions = ROIs;
+
+            obj.prepDataList = containers.Map;
 
             obj.setPath(path_to_MVGC);
 
@@ -57,9 +60,11 @@ classdef Connectivity
                     allSeedTCMat(:,sd) = mean(ds_seed.samples,2);
                 end
                 % store preprocessed data
-%                 obj.prepDataList{end + 1} = transpose(allSeedTCMat);
-               
+                if saveFlag == false
+                    obj.prepDataList(cSubj) = transpose(allSeedTCMat);
 
+                end
+               
                 % save the preprocessed data if save flag is true
                 if saveFlag == true
                     data = transpose(allSeedTCMat);
@@ -69,7 +74,7 @@ classdef Connectivity
             end
         end
 
-        function GCM(obj, GC_param_obj, actual_model_order, preprocessed_data_path)
+        function GCMLoad(obj, GC_param_obj, actual_model_order, preprocessed_data_path)
             pathOfData = fullfile(preprocessed_data_path, '/');
 
             % check whether the path directory exists or not
@@ -197,6 +202,134 @@ classdef Connectivity
             save(append(outPath, 'GC3DMat.mat'),'GC3DMat');
         end
 
+        function GCM(obj, GC_param_obj, actual_model_order )
+
+            % check whether there is preprocessed data or not
+            if isempty(obj.prepDataList)
+                return;
+            end
+
+            numberOfSubjects = length(obj.sub_range);
+            GC3DMat = nan(length(obj.regions), length(obj.regions), numberOfSubjects);
+
+            counter = 1;
+            for k = keys(obj.prepDataList)
+                %% Parameters
+                ntrials   = GC_param_obj.ntrials;     % number of trials
+                nobs      = GC_param_obj.nobs;   % number of observations per trial
+                regmode   = GC_param_obj.regmode;  % VAR model estimation regression mode ('OLS', 'LWR' or empty for default)
+                icregmode = GC_param_obj.icregmode;  % information criteria regression mode ('OLS', 'LWR' or empty for default)
+                morder    = GC_param_obj.morder;  % model order to use ('actual', 'AIC', 'BIC' or supplied numerical value)
+                momax     = GC_param_obj.momax;     % maximum model order for model order estimation
+                tstat     = GC_param_obj.tstat;     % statistical test for MVGC:  'chi2' for Geweke's chi2 test (default) or'F' for Granger's F-test
+                alpha     = GC_param_obj.alpha;   % significance level for significance test
+                mhtc      = GC_param_obj.mhtc;  % multiple hypothesis test correction (see routine 'significance')
+                seed      = GC_param_obj.seed;      % random seed (0 for unseeded)
+
+                %% Generate VAR test data
+                nvars = length(obj.regions); % number of variables
+
+                % Residuals covariance matrix.
+                SIGT = eye(nvars);
+                fprintf('\n');
+                disp(k);
+                fprintf('\n');
+                data = obj.prepDataList(k{1});
+                ptic('\n*** var_to_tsdata... ');
+%                 load(sbj);
+                X = data;
+                ptoc;
+                %name = split(listOfsubjs(n).name, '.');
+                %dirTosbj = obj.outputPath + '/GC_plots_and_outputs/' + string(name(1));
+                %mkdir(dirTosbj);
+
+                %% Model order estimation
+                % Calculate information criteria up to max model order
+                ptic('\n*** tsdata_to_infocrit\n');
+                [AIC,BIC] = tsdata_to_infocrit(X,momax,icregmode);
+                ptoc('*** tsdata_to_infocrit took ');
+                [~,bmo_AIC] = min(AIC);
+                [~,bmo_BIC] = min(BIC);
+                % Plot information criteria.
+                %                 figure(1); clf;
+                %                 plot((1:momax)',[AIC BIC]);
+                %                 saveas(gcf,dirTosbj+'/'+string(name(1))+'_IC.png')
+                %                 legend('AIC','BIC');
+                amo = actual_model_order; % actual model order
+                fprintf('\nbest model order (AIC) = %d\n',bmo_AIC);
+                fprintf('best model order (BIC) = %d\n',bmo_BIC);
+                fprintf('actual model order     = %d\n',amo);
+                % Select model order
+                if  strcmpi(morder,'actual')
+                    morder = amo;
+                    fprintf('\nusing actual model order = %d\n',morder);
+                elseif strcmpi(morder,'AIC')
+                    morder = bmo_AIC;
+                    fprintf('\nusing AIC best model order = %d\n',morder);
+                elseif strcmpi(morder,'BIC')
+                    morder = bmo_BIC;
+                    fprintf('\nusing BIC best model order = %d\n',morder);
+                else
+                    fprintf('\nusing specified model order = %d\n',morder);
+                end
+                %% Granger causality estimation
+                % Calculate time-domain pairwise-conditional causalities. Return VAR parameters
+                % so we can check VAR.
+                ptic('\n*** GCCA_tsdata_to_pwcgc... ');
+                [F,A,SIG] = GCCA_tsdata_to_pwcgc(X,morder,regmode); % use same model order for reduced as for full regressions
+                ptoc;
+                % Check for failed (full) regression
+                assert(~isbad(A),'VAR estimation failed');
+                % Check for failed GC calculation
+                assert(~isbad(F,false),'GC calculation failed');
+                % Check VAR parameters (but don't bail out on error - GCCA mode is quite forgiving!)
+                rho = var_specrad(A);
+                fprintf('\nspectral radius = %f\n',rho);
+                if rho >= 1,       fprintf(2,'WARNING: unstable VAR (unit root)\n'); end
+                if ~isposdef(SIG), fprintf(2,'WARNING: residuals covariance matrix not positive-definite\n'); end
+                % Significance test using theoretical null distribution, adjusting for multiple
+                % hypotheses.
+                pval = mvgc_pval(F,morder,nobs,ntrials,1,1,nvars-2,tstat);
+                sig  = significance(pval,alpha,mhtc);
+                % Plot time-domain causal graph, p-values and significance.
+                %                 figure(2); clf;
+                %                 subplot(1,3,1);
+                %                 plot_pw(F);
+                %                 disp(pval);
+                %                 title('Pairwise-conditional GC');
+                %                 subplot(1,3,2);
+                %                 plot_pw(pval);
+                %                 title('p-values');
+                %                 subplot(1,3,3);
+                %                 plot_pw(sig);
+                %                 title(['Significant at p = ' num2str(alpha)])
+                %                 fprintf(2,'\nNOTE: no frequency-domain pairwise-conditional causality calculation in GCCA compatibility mode!\n');
+                %                 saveas(gcf,dirTosbj+'/'+string(name(1))+'.png')
+
+                %convert p-vals to z score
+                zscore = norminv(1 - pval );
+
+                GC3DMat( :,: ,counter)= zscore;
+                counter = counter + 1; 
+%                 if counter > numberOfSubjects
+%                     disp('Subject range is not consistent with number of subjects!')
+%                     return
+%                 end
+
+                %Free up used memory for other loops
+                clear bmo_BIC bmo_AIC X figure(1) figure(2);
+            end
+
+            % save data
+            outPath = fullfile(obj.outputPath, 'GCMOutput', '/');
+
+            % check whether the path exists or not
+            if ~exist(outPath, 'dir')
+                mkdir(outPath);
+            end
+            save(append(outPath, 'GC3DMat.mat'),'GC3DMat');
+        end
+
         function GCTensor(obj, GC_param_obj, actual_model_order, preprocessed_data_path)
             if nargin == 3
                 preprocessed_data_path = nan;
@@ -209,9 +342,9 @@ classdef Connectivity
             end
 
             if loadFlag == true
-                obj.GCM(GC_param_obj, actual_model_order, preprocessed_data_path)
+                obj.GCMLoad(GC_param_obj, actual_model_order, preprocessed_data_path)
             elseif loadFlag == false
-                %obj.GCM
+                obj.GCM(GC_param_obj, actual_model_order)
             end
            
         end
